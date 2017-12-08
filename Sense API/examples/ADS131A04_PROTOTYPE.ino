86845f8252fc68d7da94e508f8b2fe2b926f356a
hhinton <hhinton@ucsc.edu>
2017-12-07 19:58:30 -0800

    Reconfiguring for introduction of Sense API

--- Sense API/examples/ADS131A04_PROTOTYPE.ino
+++ Sense API/examples/ADS131A04_PROTOTYPE.ino
@@ -1,7 +1,9 @@
 #include <SPI.h>
+#include <Filters.h>
 
-#define SPI_CLOCK_SPEED 16384000
-#define CHIP_SELECT 6
+#define SPI_CLOCK_SPEED 2000000
+#define SSP 6
+#define DRDY 4
 #define SENT 1
 #define RECEIVED 0
 
@@ -17,6 +19,7 @@
 // Response List for ADS131A04
 
 const uint32_t READY = 0xFF04;
+uint32_t COUNT = 0;
 
 // Timing List for ADS131A04
 
@@ -29,7 +32,11 @@ int RETURN_WORD_LENGTH = 3;
 // Data
 
 uint32_t CH1, CH2, CH3, CH4;
-
+uint8_t count;
+float HPFC = 1;
+float LPFC = 40;
+FilterOnePole DHPF( HIGHPASS , HPFC);
+FilterOnePole DLPF( LOWPASS,   LPFC);
 /* Setup primarily involves resetting the ADS, writing desired settings, and
  * locking registers before reading ADC data. */
 
@@ -37,41 +44,45 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Setting up...");
   SPI.begin();
+  //SPI.setClockDivider(SPI_CLOCK_DIV8);
 
-  pinMode(CHIP_SELECT, OUTPUT);
-
+  pinMode(SSP, OUTPUT);
+  pinMode(DRDY, INPUT);
   delay(1000); // Allow for startup time
-
+  //adsWriteCommand(UNLOCK, UNLOCK); // Unlock registers for setting WREG if restarting
   adsWriteCommand(RESET, READY); // Ensure POR
   adsWriteCommand(UNLOCK, UNLOCK); // Unlock registers for setting WREG
-  adsWriteRegister(0x0D, 0b10000010); // Set internal clock to use SLK
+  adsWriteRegister(0x0B, 0b11101000); // charge pump enable, internal reference enable
+  adsWriteRegister(0x0C, 0b00111100); // Set fixed frame size
+  adsWriteRegister(0x0D, 0b00000010); // Set internal clock to use OSC
   adsWriteRegister(0x0E, 0b00100000); // Set ICLK divider ratio and OSR
+  adsWriteRegister(0x11, 0b00000100);
+  adsWriteRegister(0x12, 0b00000100);
+  adsWriteRegister(0x13, 0b00000100);
+  adsWriteRegister(0x14, 0b00000100);
+  // After register settings are cleared, start ADC process
 
-  // Register Status Checks
-
-  int i;
-  for (i = 0; i < 0x14; i++) {
-    adsReadRegister(i);
+  for(count = 0; count < 0x15; count++){
+    adsReadRegister(count);
   }
 
-  
-  // After register settings are cleared, start ADC process
   adsWriteRegister(0x0F, 0b00001111); // Power on ADCs
   adsWriteCommand(WAKEUP, WAKEUP); // Wake up ADCs
-  adsWriteCommand(LOCK, LOCK); // Lock ADC
+  
+  
+  //adsWriteCommand(LOCK, LOCK); // Lock ADC
 }
 
 void loop() {
-  delay(1000);
-  adsSample(&CH1, &CH2, &CH3, &CH4); // Sample ADCS
-
+  
+  adsSample(); // Sample ADCS
+  //Serial.println();
   // Use these when checking STAT_X registers.
-  /*
-  adsWriteCommand(NULL, 0x2220);
-  adsReadRegister(0x05);
-  adsReadRegister(0x03);
+  
+  /*adsReadRegister(0x03);
   adsReadRegister(0x04);
-  */
+  adsReadRegister(0x05);*/
+  delay(10);
 }
 
 /* Message Parsing: decode received chars from ADS131A04 and
@@ -83,13 +94,14 @@ uint32_t parseM(unsigned char BUFFER[3], int SRT) {
   // Send or receive switch case
   switch (SRT) {
     case 0:
-      Serial.print("Returned: ");
+      Serial.print("R: ");
       break;
     case 1:
-      Serial.print("Sent: ");
+      Serial.print("S: ");
       break;
   }
-  Serial.print(STATUS, HEX);
+
+  printHex(STATUS, 4);
   Serial.print(" = ");
 
   // Decode cases and determine returned STATUS messages
@@ -129,7 +141,7 @@ uint32_t parseM(unsigned char BUFFER[3], int SRT) {
           Serial.println("RREG");
           break;
         default:
-          Serial.print("STATUS ERROR\n");
+          Serial.println("STATUS ERROR");
           break;
       }
       break;
@@ -141,43 +153,45 @@ uint32_t parseM(unsigned char BUFFER[3], int SRT) {
 // Write Command to ADS131A04 and get status response
 
 void adsWriteCommand(uint32_t COMMAND, uint32_t STATUS) {
-  unsigned char REC_BUF[3] = {0x00, 0x00, 0x00};
   unsigned char SND_BUF[3];
+  unsigned char REC_BUF[3];
   uint32_t RETURNED_STATUS = 0x00;
 
   Serial.println(); // Newline for serial monitor cleanliness
 
   // Begin SPI transaction using designated settings
-  SPI.beginTransaction(SPISettings(SPI_CLOCK_SPEED, MSBFIRST, SPI_MODE1));
+
 
   // Rerun transaction if status not received
   while (RETURNED_STATUS != STATUS) {
     
     // Format command into I/O buffer MSB -> first 16 bits
 
-    SND_BUF[0] = (COMMAND >> 8) & 0xFF;
+    SND_BUF[0] = (COMMAND & 0xFF00) >> 8;
     SND_BUF[1] = COMMAND & 0xFF;
     SND_BUF[2] = 0x00;
 
+    REC_BUF[0] = 0x00;
+    REC_BUF[1] = 0x00;
+    REC_BUF[2] = 0x00;
+    
     parseM(SND_BUF, SENT); // Parse received message
 
     // Enable ~CS then transfer I/O buffer, get response
-
-    digitalWrite(CHIP_SELECT, LOW);
+    SPI.beginTransaction(SPISettings(SPI_CLOCK_SPEED, MSBFIRST, SPI_MODE1));
+    digitalWrite(SSP, LOW);
     SPI.transfer(&SND_BUF, 3);
-    digitalWrite(CHIP_SELECT, HIGH);
+    digitalWrite(SSP, HIGH);
+
+    digitalWrite(SSP, LOW);
+    SPI.transfer(&REC_BUF, 3);
+    digitalWrite(SSP, HIGH);
 
     // Response is given to message on subsequent data frame
-    
-    digitalWrite(CHIP_SELECT, LOW);
-    SPI.transfer(&REC_BUF, 3);
-    digitalWrite(CHIP_SELECT, HIGH);
     SPI.endTransaction();
-
     RETURNED_STATUS = parseM(REC_BUF, RECEIVED);
   }
 
-  SPI.endTransaction();
 }
 
 
@@ -209,54 +223,87 @@ void adsReadRegister(char REG_ADDRESS) {
 
   // Enable ~CS then transfer I/O buffer, get response
 
-  digitalWrite(CHIP_SELECT, LOW);
-  delay(1);
+  digitalWrite(SSP, LOW);
   SPI.transfer(&SND_BUF, 3);
-  digitalWrite(CHIP_SELECT, HIGH);
+  digitalWrite(SSP, HIGH);
 
-  digitalWrite(CHIP_SELECT, LOW);
+  digitalWrite(SSP, LOW);
   SPI.transfer(&REC_BUF, 3);
-  digitalWrite(CHIP_SELECT, HIGH);
+  digitalWrite(SSP, HIGH);
   SPI.endTransaction();
   parseM(REC_BUF, RECEIVED);
 
 }
 
 
-void adsSample(uint32_t *CH1, uint32_t *CH2, uint32_t *CH3, uint32_t *CH4) {
-  uint8_t SIZE_BUF = 15;
-  unsigned char SND_BUF[SIZE_BUF];
+void adsSample() {
+  uint8_t BUF_SIZE = 15;
+  unsigned char SND_BUF[BUF_SIZE];
+  long CH1, CH2, CH3, CH4;
+  float CH1_f, CH2_f, CH3_f, CH4_f;
+  uint8_t i;
 
-  int i;
-
-  for(i = 0; i < SIZE_BUF; i++){
+  for(i = 0; i < BUF_SIZE; i++){
     SND_BUF[i] = 0x00;
   }
-  
-  Serial.println();
-
+  //Serial.println("DRDY");
   // Begin SPI transaction using designated settings
   SPI.beginTransaction(SPISettings(SPI_CLOCK_SPEED, MSBFIRST, SPI_MODE1));
-
-  digitalWrite(CHIP_SELECT, LOW);
-  delay(0.1);
-  SPI.transfer(&SND_BUF, SIZE_BUF);
-  digitalWrite(CHIP_SELECT, HIGH);
+  digitalWrite(SSP, LOW);
+  SPI.transfer(&SND_BUF, BUF_SIZE);
+  digitalWrite(SSP, HIGH);
   
   SPI.endTransaction();
   
-  pHex(SND_BUF, SIZE_BUF);
+  /*for(i = 0; i < BUF_SIZE; i++){
+    Serial.print(SND_BUF[i], HEX);
+  }*/
+  CH1 = (SND_BUF[3] << 16) | (SND_BUF[4] << 8) | SND_BUF[5];
+  CH2 = (SND_BUF[6] << 16) | (SND_BUF[7] << 8) | SND_BUF[8];
+  CH3 = (SND_BUF[9] << 16) | (SND_BUF[10] << 8) | SND_BUF[11];
+  CH4 = (SND_BUF[12] << 16) | (SND_BUF[13] << 8) | SND_BUF[14];
+  
+  if (CH1 & 0x00800000){
+    CH1 |= 0xFF000000 ;
+  }
+  if (CH2 & 0x00800000){
+    CH2 |= 0xFF000000 ;
+  }
+  if (CH3 & 0x00800000){
+    CH3 |= 0xFF000000 ;
+  }
+  if (CH4 & 0x00800000){
+    CH4 |= 0xFF000000 ;
+  }
+  
+  CH1_f = (CH1 / 16777216.0) * (4.80000);
+  CH2_f = (CH2 / 16777216.0) * (4.80000);
+  CH3_f = CH3 * (2* 2.40000 / 16777216);
+  CH4_f = CH4 * (2* 2.40000 / 16777216);
+  
+  /*DLPF.input(CH1_f);
+  DHPF.input(DLPF.output());*/
+  
+  Serial.print(CH1_f,24);
+  Serial.print(",");
+  Serial.print(CH2_f,24);
+  Serial.print(",");
+  Serial.print(CH3_f,24);
+  Serial.print(",");
+  Serial.println(CH4_f,24);
+  
+  //Serial.println(DHPF.output(),24);
+  COUNT++;
 }
 
-void pHex(uint8_t *data, uint8_t length) // prints 8-bit data in hex with leading zeroes
-{
-  char tmp[16];
-  for (int i = 0; i < length; i++) {
-    sprintf(tmp, "%.2X", data[i]);
-    Serial.print(tmp);
-    Serial.print(" ");
-  }
-  Serial.println();
+void printHex(int num, int precision) {
+     char tmp[16];
+     char format[128];
+
+     sprintf(format, "0x%%.%dX", precision);
+
+     sprintf(tmp, format, num);
+     Serial.print(tmp);
 }
 
 
---
+++ Sense API/src/ADC/ADS131A04/README.md
@@ -0,0 +1,23 @@
+# ADS131A04 for `Sense`
+
+TODO: Provide ADS131A04 documentation
+
+## Installation
+
+TODO: Describe the installation process
+
+## Usage
+
+TODO: Write usage instructions
+
+## History
+
+TODO: Write history
+
+## Credits
+
+TODO: Write credits
+
+## License
+
+TODO: Write license
